b6314f6c394945d105ff0d1bc90232fe1695e53e
Arseni20032 <teplyakovarseni@mail.ru>
2025-06-27 13:00:24 +0300

    added libs with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// whynothave-fun/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/components/ui/**/*.{js,ts,jsx,tsx}',
+  ],
+  plugins: [],
+};
